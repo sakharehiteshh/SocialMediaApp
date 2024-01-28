c4ee9f7bb930c12e7fa60a4157c6937dde054cd5
sakharehiteshh <sakharehiteshh@gmail.com>
2024-01-28 17:48:43 -0500

    Chat Socket

--- client/socialmediaapp/src/App.js
+++ client/socialmediaapp/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Profile from './pages/profile/Profile';
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
@@ -21,6 +22,9 @@ function App() {
         </Route>
         <Route path="/register">
         {user ? <Redirect to="/"/> : <Register />}
+        </Route>
+        <Route path="/messenger">
+        {!user ? <Redirect to="/"/> : <Messenger />}
         </Route>
         <Route path="/profile/:username">
           <Profile />
